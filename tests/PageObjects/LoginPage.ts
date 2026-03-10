add557ba1819e6e31cdab5dbd6d354d93fbb998e
Juan Fernando <JuanFer9810@gmail.com>
2026-03-10 14:55:16 -0500

    TEST LOGIN OK

--- tests/PageObjects/LoginPage.ts
+++ tests/PageObjects/LoginPage.ts
@@ -5,22 +5,24 @@ export class LoginPage{
     private readonly usernameTextbox : Locator
     private readonly passwordTextbox : Locator
     private readonly buttonLogin : Locator
-    //private readonly ButtonCheckLogin : Locator
+    private readonly ButtonCheckLogin_buttonmenu : Locator
+    private readonly ButtonCheckDefault : Locator
 
     constructor(page : Page){
         
         this.usernameTextbox = page.getByRole("textbox", {name : 'Username', exact : true})
-        this.passwordTextbox = page.getByRole("textbox", {name : 'Passwoord', exact : true})
+        this.passwordTextbox = page.getByRole("textbox", {name : 'Password', exact : true})
         this.buttonLogin = page.getByRole("button", {name : 'Login', exact : true})
-        //this.ButtonCheckLogin = page.getByRole("")
+        this.ButtonCheckLogin_buttonmenu = page.getByRole("button",{name:'Open Menu',exact:true})
+        this.ButtonCheckDefault = page.locator('//button[@class="error-button"]')
     }
 
     async fillUsername(username : string){
         await this.usernameTextbox.fill(username)
     }
 
-     async fillPassword(passwrod : string){
-        await this.passwordTextbox.fill(passwrod)
+     async fillPassword(password : string){
+        await this.passwordTextbox.fill(password)
     }
 
      async clickButtonLogin(){
@@ -33,7 +35,12 @@ export class LoginPage{
          await this.buttonLogin.click()  
     }
 
-   /* async CheckLoginSucces(){
-        await expect(this.ButtonCheckLogin).toBeVisible()
-    }*/
+   async CheckLoginSucces(){
+        await expect(this.ButtonCheckLogin_buttonmenu).toBeVisible()
+   }
+
+    async CheckLoginDefeat(){
+        await this.ButtonCheckDefault.locator('//button[@class="error-button"]').isVisible()
+        
+   }
 }
--- tests/testLogin.spec.ts
+++ tests/testLogin.spec.ts
@@ -1,11 +1,34 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from './PageObjects/LoginPage';
 
-test('testLogin', async ({ page }) => {
+test('testLoginUsergood', async ({ page },testINFO) => {
   //await page.goto(process.env.URL)
   await page.goto('https://www.saucedemo.com/');
 
  const Login = new LoginPage(page)
     await Login.loginWithCredential('standard_user','secret_sauce')
-    await page.pause()
+    await Login.CheckLoginSucces()
+
+      // captura que se adjunte al reporte 
+    await testINFO.attach('login',{
+        body: await page.screenshot(),
+        contentType: 'image/png'
+    })
+
+});
+
+test('testLoginUserbad', async ({ page },testINFO) => {
+  //await page.goto(process.env.URL)
+  await page.goto('https://www.saucedemo.com/');
+
+ const Login = new LoginPage(page)
+    await Login.loginWithCredential('standard_user','password_bad')
+    await Login.CheckLoginDefeat()
+
+      // captura que se adjunte al reporte 
+    await testINFO.attach('login',{
+        body: await page.screenshot(),
+        contentType: 'image/png'
+    })
+
 });
