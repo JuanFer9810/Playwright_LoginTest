32ebbfa315ecf664898300471fd42dd821278d0a
Felipe Pérez <felipeperez@MacBook-Pro-de-Felipe.local>
2026-03-10 16:57:39 -0500

    Comments: Revision local del codigo, se dejan los comentarios con las mejoras para realizar solo del codigo

--- tests/PageObjects/LoginPage.ts
+++ tests/PageObjects/LoginPage.ts
@@ -9,7 +9,8 @@ export class LoginPage{
     private readonly ButtonCheckDefault : Locator
 
     constructor(page : Page){
-        
+        // Buena nomenclatura de las variables, puntos extra por camelCase y nomenclatura descriptiva pero si empieza con camelCase no mezcle 
+        // snake_case: ButtonCheckLogin_buttonmenu mejor ponerle algo asi como SuccessfulLoginButton 
         this.usernameTextbox = page.getByRole("textbox", {name : 'Username', exact : true})
         this.passwordTextbox = page.getByRole("textbox", {name : 'Password', exact : true})
         this.buttonLogin = page.getByRole("button", {name : 'Login', exact : true})
@@ -28,19 +29,24 @@ export class LoginPage{
      async clickButtonLogin(){
         await this.buttonLogin.click()
     }
-
+    //Error de misspelled, enredar las palabras no es buena practica y muchos herramientas en el pipeline fallan por esas bobadas
+    // Pero funciona bien password
     async loginWithCredential(email:string,passwrod:string){
          await this.fillUsername(email)
          await this.fillPassword(passwrod)
          await this.buttonLogin.click()  
     }
-
+        // Si el metodo se llama checkLoginDefeat, se espera que el boton de error sea visible, el isVisible() solo devuelve un booleano pero se usa para otras cosas entonces en el 
+        // escenario del caso fallido no esta haciendo nada hace ir a la pagina, llenar los datos, y true. Entonces cambialo por el otro metodo tobeVisible() que ahi si valida que diga el mensaje de error de login o lo que sea
+        // Por gramatica en lugar de CheckLoginDefeat, se podria llamar ValidateLoginError. Ese Defeat y el check no son tan buenas traducciones. Mejor Validate y como en testing necesitamos
+        // que las cosas sean menos genericas y mas especificas, se podria llamar ValidateLoginErrorMessageIsVisible 
+        // Lo mismo para el CheckLoginSucces, se podria llamar ValidateLoginSuccessMessage. Funciona uno si, el otro no. Y gramatica mal ambos. 
+        
    async CheckLoginSucces(){
         await expect(this.ButtonCheckLogin_buttonmenu).toBeVisible()
    }
 
     async CheckLoginDefeat(){
         await this.ButtonCheckDefault.locator('//button[@class="error-button"]').isVisible()
-        
    }
 }
--- tests/testLogin.spec.ts
+++ tests/testLogin.spec.ts
@@ -1,8 +1,20 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from './PageObjects/LoginPage';
+// Esto si es por que se me olvido enseñarlo pero ese man de Julian Mesa debe tener videos de nomenclatura Gherkin, que 
+// es lo que mas se usa para pruebas en general se dice que las pruebas son por feature en espanol es como un conjunto de funcionalidades
+// en este caso iniciar sesion entonces en playwright debe existir algo como describe('Inicio de Sesion', () => { })
+// para decirle al compilador todo este bloque son pruebas de inicio de sesion, de hecho no le debio de haber corrido sin eso pero bueno la magia de un buen framework
+// con eso tambien para cada test debe ser descriptivo y obvio en letra despegada el nombre del test debe ser autodescriptivo y completo no importa que se extienda un poquito
+// test('Validate Successful Login With Valid Credentials', por ejemplo 
+// test('Validate An Error Message is Shown When Using Invalid Credentials',
+// Puntos porque funciona bien el caso positivo, pero el negativo ahi le deje el comentario en el metodo de porque no valida nada
 
 test('testLoginUsergood', async ({ page },testINFO) => {
   //await page.goto(process.env.URL)
+  // No se si en los videos estaba pero si usa el env para la url, no lo ponga quemado
+// page.goto(process.env.URL) pero con cuidado es breve configurarlo en el archivo de playwight.config.ts 
+// linea 9 creo que es el path del .env y por seguridad hay mucha data tokens, secrets, url, cedulas, tarjetas de credito etc etc que se manejan por .env 
+// puntos por intentar usarlo pero pillese bien como configurarlo 
   await page.goto('https://www.saucedemo.com/');
 
  const Login = new LoginPage(page)
@@ -26,6 +38,10 @@ test('testLoginUserbad', async ({ page },testINFO) => {
     await Login.CheckLoginDefeat()
 
       // captura que se adjunte al reporte 
+      // bien por lo del reporte pero ese Julian mesa es un bobo hp para los reportes despues le enseno a crearlos bien
+      // y normalmente eso va en tareas o hooks que se ejecutan despues de los tests, algo asi como 
+      // test.afterEach(async ({ page }, testINFO) => { entonces en espanol es como despues de cada test tome los screenshot, incluso eso se pone 
+      // es depues de cada paso del test o despues de cada interaccion con la pagina. Se doy puntos por proactivo pero despues se los enseno melos
     await testINFO.attach('login',{
         body: await page.screenshot(),
         contentType: 'image/png'
